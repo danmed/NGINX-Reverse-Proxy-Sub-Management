8db0e6f127e8b34e5473676ab576b033f5895385
danmed <danmed@gmail.com>
2017-12-05 14:28:10 +0000

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+apt update -y
+apt install nginx -y
+cd /etc/nginx/sites-available
+wget https://raw.githubusercontent.com/danmed/NGINX-Reverse-Proxy-Sub-Management/master/new
+cd
+wget https://raw.githubusercontent.com/danmed/NGINX-Reverse-Proxy-Sub-Management/master/deletedomain.sh
+wget https://raw.githubusercontent.com/danmed/NGINX-Reverse-Proxy-Sub-Management/master/newdomain.sh
+chmod 755 newdomain.sh
+chmod 755 deletedomain.sh
