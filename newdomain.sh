45b25ae70bcdade66c3ef6b431fde3d55cb582e9
danmed <danmed@gmail.com>
2017-12-05 13:23:18 +0000

    Update newdomain.sh

--- newdomain.sh
+++ newdomain.sh
@@ -1,17 +1,18 @@
 #!/bin/bash
+MYDOMAIN=MYDOMAIN.co.uk
 echo "**************************************************"
 echo "**         New NGINX Sub Domain Generator       **"
 echo "**************************************************"
-echo "Enter name of new subdomain e.g test.MYDOMAIN.co.uk:"
+echo "Enter name of new subdomain e.g test.$MYDOMAIN.co.uk:"
 read sub
 echo "Enter the internal IP Address of the host:"
 read ip
 echo "Enter the port number"
 read port
-cp /etc/nginx/sites-available/new /etc/nginx/sites-available/$sub.danmed.co.uk
-sed -i "s/new/$sub/" "/etc/nginx/sites-available/$sub.danmed.co.uk"
-sed -i "s/<ipaddress>/$ip/" "/etc/nginx/sites-available/$sub.danmed.co.uk"
-sed -i "s/<port>/$port/" "/etc/nginx/sites-available/$sub.danmed.co.uk"
-ln -s /etc/nginx/sites-available/$sub.danmed.co.uk /etc/nginx/sites-enabled/$sub.danmed.co.uk
+cp /etc/nginx/sites-available/new /etc/nginx/sites-available/$sub.$MYDOMAIN
+sed -i "s/new/$sub/" "/etc/nginx/sites-available/$sub.$MYDOMAIN"
+sed -i "s/<ipaddress>/$ip/" "/etc/nginx/sites-available/$sub.$MYDOMAIN"
+sed -i "s/<port>/$port/" "/etc/nginx/sites-available/$sub.$MYDOMAIN"
+ln -s /etc/nginx/sites-available/$sub.$MYDOMAIN /etc/nginx/sites-enabled/$sub.$MYDOMAIN
 systemctl restart nginx
 echo $sub".danmed.co.uk created and enabled"
