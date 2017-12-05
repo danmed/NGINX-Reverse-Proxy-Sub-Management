e8d26b2f3bb4e82b74031868bfdf6f05de85d55b
danmed <danmed@gmail.com>
2017-12-05 13:20:23 +0000

    Update deletedomain.sh

--- deletedomain.sh
+++ deletedomain.sh
@@ -4,7 +4,7 @@ echo "**           Delete NGINX Sub Domain            **"
 echo "**************************************************"
 echo "Enter name of subdomain e.g test:"
 read sub
-rm /etc/nginx/sites-available/$sub.danmed.co.uk
-rm /etc/nginx/sites-enabled/$sub.danmed.co.uk
+rm /etc/nginx/sites-available/$sub.MYDOMAIN.co.uk
+rm /etc/nginx/sites-enabled/$sub.MYDOMAIN.co.uk
 systemctl restart nginx
 echo $sub".danmed.co.uk removed from config"
