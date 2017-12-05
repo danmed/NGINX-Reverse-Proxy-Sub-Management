556c54bd9d5da93e0502f424601232c563b23cc4
danmed <danmed@gmail.com>
2017-12-05 14:29:04 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 apt update -y
+apt upgrade -y
 apt install nginx -y
 cd /etc/nginx/sites-available
 wget https://raw.githubusercontent.com/danmed/NGINX-Reverse-Proxy-Sub-Management/master/new
